60e4ea32076436a56ea9fa24a1fa65e00c2e7845
Caleb Diehl <cdiehl@ap.org>
2023-03-19 15:47:20 -0700

    remove select

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import Chart from './Chart'
 import sunshine from './sunshine.json'
-import Select from 'react-select'
-import { useState } from 'react'
-
-const options = [
-  { value: 'JUL', label: 'July' },
-  { value: 'JUN', label: 'June' },
-  { value: 'AUG', label: 'August' },
-]
 
 function App() {
-  const [month, setMonth] = useState(options[0])
   const data = sunshine
     .map((d) => {
-      return { city: d.CITY, sunshine: d[month.value] }
+      return { city: d.CITY, sunshine: d['JUL'] }
     })
     .sort((a, b) => b.sunshine - a.sunshine)
     .slice(0, 20)
@@ -21,10 +12,7 @@ function App() {
   return (
     <div className='App'>
       <div className='container'>
-        <div className='header'>
-          <h1>Sunshine By City</h1>
-          <Select defaultValue={month} onChange={setMonth} options={options} />
-        </div>
+        <div className='header'></div>
 
         <Chart data={data} width={700} height={500} />
       </div>
