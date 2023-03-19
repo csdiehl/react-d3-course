40e10e5970bc6114b5f790d0e29cc186611f1a03
Caleb Diehl <cdiehl@ap.org>
2023-03-19 16:54:53 -0700

    animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.css'
 import Circles from './Circles'
+import data from './gapminder_data.json'
+import { useState } from 'react'
+import Slider from 'react-input-slider'
+import { scaleOrdinal, schemeTableau10 } from 'd3'
 
-var width = 960
-var height = 500
+const width = 960
+const height = 500
+let colors = ['#2176ae', '#57b8ff', '#b66d0d', '#fbb13c', '#fe6847']
 
 function App() {
+  const [state, setState] = useState({ x: 1957 })
+  const filteredData = data.filter((d) => d.year === state.x)
+  const continents = [...new Set(data.map((d) => d.continent))]
+
+  const color = scaleOrdinal().domain(continents).range(schemeTableau10)
+
   return (
     <div className='App'>
-      <Circles width={width} height={height} />
+      <h1>Gapminder Chart</h1>
+      <Circles
+        width={width}
+        height={height}
+        data={filteredData}
+        colorScale={color}
+      />
+      <h2>{state.x}</h2>
+      <Slider
+        axis='x'
+        xmin={1957}
+        xmax={2007}
+        xstep={5}
+        x={state.x}
+        onChange={({ x }) => setState((state) => ({ ...state, x }))}
+      />
     </div>
   )
 }
--- src/Circles.js
+++ src/Circles.js
@@ -1,50 +1,48 @@
 import './index.css'
-import { scaleLinear, select } from 'd3'
-import { useEffect, useState, useRef } from 'react'
-import { getData } from './utils'
+import { scaleLinear, select, max, extent, scaleSqrt } from 'd3'
+import { useEffect, useRef } from 'react'
 
-let colors = ['#2176ae', '#57b8ff', '#b66d0d', '#fbb13c', '#fe6847']
+// https://observablehq.com/@uwdata/introduction-to-d3-part-2
 
-function Circles({ width, height }) {
+const margin = { left: 20, right: 20, top: 30, bottom: 30 }
+
+function Circles({ width, height, data, colorScale }) {
   // store the data in state
-  const [data, setData] = useState(getData())
 
   // store a Ref to the SVG that will allow d3 to access the DOM element
   const svgRef = useRef()
 
-  // when button clicked, randomly update the data stored in state, triggering a re-render
-  const handleClick = () => setData(getData())
-
   // when data or dimensions change, update the circle attributes - color, size, and position
   useEffect(() => {
     let maxRadius = 40
-    let xScale = scaleLinear().domain([0, 1]).range([0, width])
-    let yScale = scaleLinear().domain([0, 1]).range([0, height])
-    let rScale = scaleLinear().domain([0, 1]).range([0, maxRadius])
+    let xScale = scaleLinear()
+      .domain([0, max(data, (d) => d.gdp_cap)])
+      .range([margin.left, width - margin.right])
+    let yScale = scaleLinear()
+      .domain([margin.top, max(data, (d) => d.life_exp)])
+      .range([height - margin.bottom, margin.top])
+    let rScale = scaleSqrt()
+      .domain(extent(data, (d) => d.population))
+      .range([1, maxRadius])
 
     select(svgRef.current)
       .selectAll('circle')
       .data(data)
       .transition()
-      .duration(1000)
-      .attr('cx', (d) => xScale(d.x))
-      .attr('cy', (d) => yScale(d.y))
-      .attr('r', (d) => rScale(d.r))
-      .style('fill', (d) => colors[d.color])
-  }, [data, height, width])
+      .duration(500)
+      .attr('cx', (d) => xScale(d.gdp_cap))
+      .attr('cy', (d) => yScale(d.life_exp))
+      .attr('r', (d) => rScale(d.population))
+      .style('fill', (d) => colorScale(d.continent))
+  }, [data, height, width, colorScale])
 
   // this just attaches circles to the DOM - it doesn't actually set their size, color, or position
   return (
-    <div>
-      <svg ref={svgRef} viewBox={`0 0 ${width} ${height}`}>
-        {data.map((d) => (
-          <circle fill='#FFF' />
-        ))}
-      </svg>
-      <div>
-        <button onClick={handleClick}>Refresh Data</button>
-      </div>
-    </div>
+    <svg ref={svgRef} viewBox={`0 0 ${width} ${height}`}>
+      {data.map((d) => (
+        <circle key={d.country} fill='#FFF' />
+      ))}
+    </svg>
   )
 }
 
