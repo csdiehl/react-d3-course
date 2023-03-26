65c4cfbdea435f04c59e50349e08e7c93cdbd1cd
Caleb Diehl <cdiehl@ap.org>
2023-03-26 12:54:16 -0700

    delete use effect code

--- src/Circles.js
+++ src/Circles.js
@@ -9,29 +9,8 @@ import "./index.css"
 const margin = { left: 50, right: 20, top: 30, bottom: 50 }
 
 function Circles({ width, height, year, colorScale, selectedContinent }) {
-  // filter the data to the selected year
-  const chartData = data.filter((d) => d.year === year)
-  // store a Ref to the SVG that will allow d3 to access the DOM element
-  const svgRef = useRef()
-
-  // when data or dimensions change, update the circle attributes - color, size, and position
-  useEffect(() => {
-    const SVG = select(svgRef.current)
-    // scales
-    drawChart(
-      SVG,
-      chartData,
-      data,
-      height,
-      width,
-      margin,
-      colorScale,
-      selectedContinent
-    )
-  }, [chartData, height, width, colorScale, selectedContinent])
-
   // this just attaches circles to the DOM - it doesn't actually set their size, color, or position
-  return <svg ref={svgRef} viewBox={`0 0 ${width} ${height}`}></svg>
+  return <svg viewBox={`0 0 ${width} ${height}`}></svg>
 }
 
 export default Circles
