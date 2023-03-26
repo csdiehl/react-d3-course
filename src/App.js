9bce62871c9fc2bc9f6dead43d95b9bfe13ba3e0
Caleb Diehl <cdiehl@ap.org>
2023-03-26 12:51:45 -0700

    remove scales

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import "./App.css"
 import Circles from "./Circles"
-import data from "./gapminder_data.json"
-import { scaleOrdinal, schemeTableau10 } from "d3"
 
 const width = 960
 const height = 500
 
 function App() {
-  const continents = [...new Set(data.map((d) => d.continent))]
-
-  const color = scaleOrdinal().domain(continents).range(schemeTableau10)
-
   return (
     <div className="App">
       <h1 className="header">Gapminder Chart</h1>
       <div className="slider"></div>
-
       <div className="chart">
-        <Circles year={1957} width={width} height={height} colorScale={color} />
+        <Circles year={1957} width={width} height={height} />
       </div>
     </div>
   )
--- src/drawChart.js
+++ src/drawChart.js
@@ -1,5 +1,3 @@
-import { extent, scaleLinear, scaleLog, scaleSqrt } from "d3"
-
 export function drawChart(
   SVG,
   chartData,
@@ -9,16 +7,4 @@ export function drawChart(
   margin,
   colorScale,
   selectedContinent
-) {
-  // scales
-  let maxRadius = 40
-  let xScale = scaleLog()
-    .domain(extent(data, (d) => d.gdp_cap))
-    .range([margin.left, width - margin.right])
-  let yScale = scaleLinear()
-    .domain(extent(data, (d) => d.life_exp))
-    .range([height - margin.bottom, margin.top])
-  let rScale = scaleSqrt()
-    .domain(extent(data, (d) => d.population))
-    .range([1, maxRadius])
-}
+) {}
